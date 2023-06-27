078ff6bbaab0d37f842cba43b838a52148d71597
Hirwa Rukundo Hope <gakundohope5@gmail.com>
2023-06-27 20:05:46 +0200

    added data validation with joi function

--- backend/Models/user.js
+++ backend/Models/user.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose');
-// user.model.js
-
-const mongoose = require('mongoose');
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  fullName: { type: String,required: true, unique: false},
+  // Additional properties can be added as per your requirements
+  fullName: { type: String },
   phoneNumber: { type: String },
+  // ...
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+// Validation function
+const validateUser = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().required().label('Username'),
+    email: Joi.string().email().required().label('Email'),
+    password: passwordComplexity().required().label('Password'),
+    fullName: Joi.string().label('Full Name'),
+    phoneNumber: Joi.string().label('Phone Number'),
+    // ...
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = {
+  User,
+  validateUser,
+};
