1e7f49d5a1c996526d451f5dfa69359d74a48461
BYUMVUHOREAimable <122086815+BYUMVUHOREAimable@users.noreply.github.com>
2023-07-01 07:16:20 +0200

    updating Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,16 @@ import React from 'react';
 
 export default function Login() {
   return (
-    
-
-          <div className='bg-gray-800 flex flex-col justify-center'>
+    <div className='bg-gray-800 flex flex-col justify-center'>
             <form className='max-w-[400px] w-full mx-auto rounded-lg p-8 px-8'>
                 <h2 className='text-4xl dark:text-white font-bold text-center mt-1'>Hello! <br /> Welcome back.</h2>
-                <div className='flex flex-col text-gray-400 py-2 mt-9'>
+                <div className='flex flex-col text-gray-400 py-2 mt-5'>
                     <label>Email address</label>
-                    <input className='mt-5 p-2 rounded-lg focus:border-blue-500 border-stone-200 bg-slate-600 text-center' type="text" placeholder='johndoe@example.com' />
+                    <input className='mt-3 p-2 rounded-lg focus:border-blue-500 border-stone-200 bg-slate-600 text-center' type="text" placeholder='johndoe@example.com' />
                 </div>
                 <div className='flex flex-col text-gray-400 py-2'>
                     <label>Password</label>
-                    <input className='mt-5 p-2 rounded-lg focus:border-blue-500 border-stone-200 bg-slate-600 text-center' type="password" placeholder='........'/>
+                    <input className='mt-3 p-2 rounded-lg focus:border-blue-500 border-stone-200 bg-slate-600 text-center' type="password" placeholder='........'/>
                 </div>
                 <div className='flex justify-between text-gray-400 py-2'>
                     <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
