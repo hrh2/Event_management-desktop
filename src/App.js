d432f29fabad7bac7a7d5f29d5fd762dddc2ff72
Hirwa Rukundo Hope <gakundohope5@gmail.com>
2023-07-11 09:14:44 +0200

    home page components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css'
 import ResponsivePage from './components/ResponsivePage';
 import { Route, Routes } from 'react-router-dom'
 import DesktopLanding from './components/DesktopLanding';
@@ -9,6 +10,9 @@ import ForgotPassword from './components/ForgotPassword'
 import AutheLayout from './components/AutheLayout';
 import Homepage from './components/SubComponents/Homepage';
 import HomepagePhoto from './components/SubComponents/HomepagePhoto';
+import CategoryCard from './components/SubComponents/CategoryCard';
+import Searchbar from './components/SubComponents/Searchbar';
+import EventCard from './components/SubComponents/EventCard';
 import BookPage from './components/SubComponents/BookPage';
 function App() {
   return (
@@ -16,7 +20,10 @@ function App() {
       <Route path='/BookPage' exact element={<BookPage />} />
       <Route path='/Homepage' exact element={<Homepage />} />
       <Route path='/HomepagePhoto' exact element={<HomepagePhoto/>} />
-      
+      <Route path='/CategoryCard' exact element={<CategoryCard/>} />
+      <Route path='/EventCard' exact element={<EventCard/>} />
+      <Route path='/Searchbar' exact element={<Searchbar/>} />
+
       <Route path='/' exact element={<DesktopLanding/>} />
       <Route path='/enteraccount' exact element={<ResponsivePage/>} />
       <Route path='/authentication' exact element={<AutheLayout />}>
