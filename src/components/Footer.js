f75de681c89cc1f8be4c47d98b204b3f105c079a
hirwa <gakundohope5@gmail.com>
2023-09-03 01:18:31 +0200

    new files

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+      <footer className="py-4 bg-slate-100 text-gray-600 text-center">
+          <p>&copy; EventConnect 2023</p>
+      </footer>
+  )
+}
